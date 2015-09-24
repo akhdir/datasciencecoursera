9228a956af1cac5906a0faf1b0f3b296e37ac704
Aboud <akhdir@souq.com>
2015-09-23 21:57:27 -0400

    removing un needed files

--- code/course3/project/code/w3-project.R
+++
@@ -1,7 +0,0 @@
-
-
-# converting data frame to vector
-as.vector(t(s))
-
-# repeating vector 
-rep(1:4, each=561)
--- code/course3/week2/code/q1.R
+++
@@ -1,5 +0,0 @@
-library(httr);
-html = GET("https://api.github.com/users/jtleek/repos");
-json1 = content(html);
-json2 = jsonlite::fromJSON(toJSON(json1));
-json2[json2$name == 'datasharing',]$created_at;
